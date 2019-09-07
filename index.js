9972e698ef05c79e586b7e27f0c15b642fd5664e
jasonmatthewgarcia <jason.garcia.010@gmail.com>
2019-09-08 01:05:04 +0800

    Initial commit personal website

---
+++ index.js
@@ -0,0 +1,23 @@
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+var exit = document.getElementById('exit');
+
+menu.addEventListener('click', function(e) {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+});
+
+exit.addEventListener('click', function(e) {
+    nav.classList.add('hide-mobile');
+        e.preventDefault();
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
